574b43de47ecf929f25095507907d9e27aed0be4
Jan Klausa <jan@klausa.pl>
2024-01-21 22:22:30 +0900

    add script to change displays to desktop

---
+++ scripts/switch-screens-to-laptop.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="F4F74892-D26E-4725-BB37-7620CEB43262" -feature="ddc" -vcp="inputSelect" -value="15"
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="C99BD8B4-296E-45BC-BB72-E13BD7592D38" -feature="ddc" -vcp="inputSelect" -value="16"
