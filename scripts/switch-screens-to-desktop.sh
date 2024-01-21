f68476bf24d018256c441873c493868250c4da6e
Jan Klausa <jan@klausa.pl>
2024-01-21 22:20:59 +0900

    add scripts to change displays to desktop

---
+++ scripts/switch-screens-to-desktop.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# lg ultra
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="835B09F5-8FDB-40DD-A49C-3F68320AB449" -feature="ddc" -vcp="inputSelect" -value="17"
+# m28u
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="A6C8C6BB-CF19-48D2-840B-08CFD3E86706" -feature="ddc" -vcp="inputSelect" -value="15"
