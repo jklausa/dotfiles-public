28093dc06d3bc5a501c554a647006601c26723b0
Jan Klausa <jan@klausa.pl>
2024-01-22 00:25:44 +0900

    silence the scripts again

--- scripts/switch-screens-to-desktop.sh
+++ scripts/switch-screens-to-desktop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # lg ultra
-/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -name="LG Ultra HD" -feature="ddc" -vcp="inputSelect" -value="17" #>/dev/null 2>&1
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -name="LG Ultra HD" -feature="ddc" -vcp="inputSelect" -value="17" >/dev/null 2>&1
 # m28u
-/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -name="M28U" -feature="ddc" -vcp="inputSelect" -value="15" #>/dev/null 2>&1
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -name="M28U" -feature="ddc" -vcp="inputSelect" -value="15" >/dev/null 2>&1
