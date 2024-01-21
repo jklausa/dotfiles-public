c33b5c1369da89be57df8c6dfdcbb6f1114f5dff
Jan Klausa <klausa@mercari.com>
2024-01-22 00:18:06 +0900

    switch to using names for displays for portability (different macs will have different UUIDs)

--- scripts/switch-screens-to-desktop.sh
+++ scripts/switch-screens-to-desktop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # lg ultra
-/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="835B09F5-8FDB-40DD-A49C-3F68320AB449" -feature="ddc" -vcp="inputSelect" -value="17" >/dev/null 2>&1
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -name="LG Ultra HD" -feature="ddc" -vcp="inputSelect" -value="17" #>/dev/null 2>&1
 # m28u
-/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="A6C8C6BB-CF19-48D2-840B-08CFD3E86706" -feature="ddc" -vcp="inputSelect" -value="15" >/dev/null 2>&1
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -name="M28U" -feature="ddc" -vcp="inputSelect" -value="15" #>/dev/null 2>&1
