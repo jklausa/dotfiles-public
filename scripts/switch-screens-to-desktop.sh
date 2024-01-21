b31d4bc45214f436c40d60f41b9cfdbda5a5bd03
Jan Klausa <jan@klausa.pl>
2024-01-21 22:31:47 +0900

    silence output from the switching scripts

--- scripts/switch-screens-to-desktop.sh
+++ scripts/switch-screens-to-desktop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # lg ultra
-/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="835B09F5-8FDB-40DD-A49C-3F68320AB449" -feature="ddc" -vcp="inputSelect" -value="17"
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="835B09F5-8FDB-40DD-A49C-3F68320AB449" -feature="ddc" -vcp="inputSelect" -value="17" >/dev/null 2>&1
 # m28u
-/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="A6C8C6BB-CF19-48D2-840B-08CFD3E86706" -feature="ddc" -vcp="inputSelect" -value="15"
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="A6C8C6BB-CF19-48D2-840B-08CFD3E86706" -feature="ddc" -vcp="inputSelect" -value="15" >/dev/null 2>&1
--- scripts/switch-screens-to-laptop.sh
+++ scripts/switch-screens-to-laptop.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="F4F74892-D26E-4725-BB37-7620CEB43262" -feature="ddc" -vcp="inputSelect" -value="15"
-/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="C99BD8B4-296E-45BC-BB72-E13BD7592D38" -feature="ddc" -vcp="inputSelect" -value="16"
+# lg
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="F4F74892-D26E-4725-BB37-7620CEB43262" -feature="ddc" -vcp="inputSelect" -value="15" >/dev/null 2>&1
+# m28u
+/Applications/BetterDisplay.app/Contents/MacOS/BetterDisplay set -UUID="C99BD8B4-296E-45BC-BB72-E13BD7592D38" -feature="ddc" -vcp="inputSelect" -value="16" >/dev/null 2>&1
